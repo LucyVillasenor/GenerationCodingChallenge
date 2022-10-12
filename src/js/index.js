251c0a27f38d0b8f42f4d647b6e9cfd334288192
LucyVillasenor <lucia.auroravp@gmail.com>
2022-10-12 15:02:16 -0500

    -Se añade la llave para utilizar GoogleMaps

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+let map;
+const mapDiv = document.getElementById("map");
+function initMap() {
+    map = new google.maps.Map(mapDiv,{
+    center: { lat: 23.0000000, lng: -102.0000000 },
+    zoom: 6,
+  });
+}
+
+// window.initMap = initMap;
